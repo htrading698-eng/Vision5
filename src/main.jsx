19c79c8e77f0529b889bbff688961246c3dbb28b
htrading698-eng <htrading698@gmail.com>
2026-05-26 22:15:49 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null }
+  }
+
+  componentDidCatch(error) {
+    this.setState({ error })
+  }
+
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ padding: 20, color: 'white', background: '#05010a', minHeight: '100vh' }}>
+          <h1>Erreur App.jsx</h1>
+          <pre>{String(this.state.error)}</pre>
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 )
